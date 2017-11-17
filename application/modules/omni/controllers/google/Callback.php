505812e0b71e74e0ac423cf81a4bed5405244ddb
Tupay <lutfi@mhs.ubharajaya.ac.id>
2017-11-18 01:43:27 +0700

    update google login

--- application/modules/omni/controllers/google/Callback.php
+++ application/modules/omni/controllers/google/Callback.php
@@ -10,8 +10,7 @@ class Callback extends CI_Controller {
 		* ini adalah callback dari oauth facebook. 
 		* jika digunakan di front end harus ada treathment khusus.
 		*/ 
-		if (isset($_GET['code'])) {
-			// http://localhost:8888/ibiddevelopment/ibiddevapi/ibidadmsuser/index.php/omni/google/callback?code=4/iuwDY-oakWyJ46Kw4f1GTAlaXLoTzwmuS1_QwJAOJVo#
+		if (isset($_GET['code'])) { 
 			$this->googleplus->getAuthenticate();  
 			// print_r($this->googleplus->getUserInfo());
 			$data = $this->googleplus->getUserInfo(); 
@@ -25,7 +24,31 @@ class Callback extends CI_Controller {
 			$this->session->set_userdata( $array );
 
 
-			redirect('welcome','refresh');
+			############################################################
+			$res = $this->proses(json_encode( ['email' => @$data['email']] ));
+			$data = json_decode($res, TRUE);
+
+				// print_r($data);
+				// exit();
+
+			if ($data['status'] === 200) { 
+
+				$this->session->set_flashdata('message', "<script type='text/javascript'> swal('Good job!', '" . $data['message'] . "', 'success'); </script>");
+				// menambahkan untuk session
+				$array = array(
+					'userdata' => array_merge(@$data['data']['0'] , @$data['data']['1'])
+				);
+				$this->session->set_userdata( $array );
+
+				redirect('dashboard','refresh');
+				exit();
+			}  else {
+				$this->session->set_flashdata('message', "<script type='text/javascript'> swal('Uuuh !', '" . $data['message'] . "', 'error'); </script>");
+
+				redirect('auth/login','refresh');
+			} 
+			##############################################################
+
 
 		} else {
 			//mengembalikan ke auth register
@@ -33,6 +56,26 @@ class Callback extends CI_Controller {
 			// redirect('afterlogin','refresh');
 		}
 		
+	}
+
+
+	public function proses($parameter = '')
+	{
+		// $this->output->set_content_type('application/json');
+		$parameter;
+		// exit();
+
+		// echo "berhasil";
+		// print_r($parameter);
+		// $json = file_get_contents('php://input');
+  		// $dataInsert = json_decode($json, TRUE); 
+
+		$url = linkservice('stoksis') ."api/accounts/loginSosmed/";
+		$method = 'POST';
+		$responseApi = ngeCurl($url, $parameter, $method);
+
+		return $responseApi;
+
 	}
 
 }
